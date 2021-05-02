59d5a84b3e93a58ab82571825d03a6737c126d9f
Roman-Schachta <71693811+Roman-Schachta@users.noreply.github.com>
2021-05-02 18:27:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "aws_region" {
   type        = string
   default     = "us-west-2"
 }
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_count" {
+  description = "Number of instances to provision."
+  type        = number
+  default     = 2
+}
